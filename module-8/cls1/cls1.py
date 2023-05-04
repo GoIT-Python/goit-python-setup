ffdb7cdb22c44d4c2d0f5717620c39a1c313a395
Volodymyr Kurov <vokur13@gmail.com>
2023-05-04 18:42:05 +0300

    timedelta +

--- module-8/cls1/cls1.py
+++ module-8/cls1/cls1.py
@@ -11,3 +11,14 @@ print(current_time.time())
 
 custom_dt = datetime(year=1970, month=4, day=22)
 print(custom_dt)
+print(custom_dt.weekday())
+
+custom_dt1 = datetime(year=1970, month=4, day=22)
+custom_dt2 = datetime(year=1992, month=4, day=25)
+
+result=custom_dt2-custom_dt1
+print(result.days)
+
+
+
+
