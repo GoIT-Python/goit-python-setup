94ea4b657f37a81ec4f7be79a261a26c5504a0f1
Volodymyr Kurov <vokur13@gmail.com>
2023-05-06 12:50:02 +0300

    task4 under construction

---
+++ module-8/task4/task4.py
@@ -0,0 +1,28 @@
+from random import randrange
+
+min=1
+max=49
+quantity=6
+
+def get_numbers_ticket(min, max, quantity):
+    if not min > 0:
+        return []
+    elif not max < 1001:
+        return []
+    elif not min < quantity < max:
+        return []
+
+    numbers=[]
+    i=min
+    while i<=quantity:
+        number= randrange(min, max+1)
+        if not number in numbers:
+            numbers.append(number)
+            i+=1
+        else:
+            continue
+        res=sorted(numbers)
+        return res
+
+
+get_numbers_ticket(min,max,quantity)
