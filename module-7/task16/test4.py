f2ed7e779fb33056f4d20465e93a193303664c65
Volodymyr Kurov <vokur13@gmail.com>
2023-05-10 15:28:54 +0300

    module-9 tasks +

---
+++ module-7/task16/test4.py
@@ -0,0 +1,14 @@
+data = ["X", 3, "Z", 2, "X", 2, "Y", 3, "Z", 2]
+
+
+def decode(data):
+    if not (bool(data)):
+        return []
+    list = []
+    i=0
+    while True:
+
+
+
+res = decode(data)
+print(res)
